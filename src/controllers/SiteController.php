47da78125ea1279b7e2e99a64849af459ca5923c
caoyufei <atcaoyufei@gmail.com>
2022-12-20 15:59:28 +0800

    0.1

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -11,7 +11,7 @@ class SiteController extends AppController {
         $about = Post::findOne(['alias' => 'about']);
         $duration = 3600;
         $newsList = Post::find()->where(['type' => Post::TYPE_NEWS])->cache($duration)->limit(4)->orderBy('id desc')->asArray()->all();
-        $productList = Post::find()->where(['type' => Post::TYPE_PRODUCT])->cache($duration)->limit(10)->orderBy('id desc')->asArray()->all();
+        $productList = Post::find()->where(['type' => Post::TYPE_PRODUCT])->cache($duration)->limit(20)->orderBy('id desc')->asArray()->all();
 
         return $this->render(compact('about', 'newsList', 'productList'));
     }
